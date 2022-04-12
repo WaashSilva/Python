764ec72d288867a00a30d84c1a00f966e60f4696
WaashSilva <98480388+WaashSilva@users.noreply.github.com>
2022-04-12 14:01:15 +0100

    Delete Basico directory

--- Basico/porcentagemdedesconto.py
+++
@@ -1,6 +0,0 @@
-print ('Ola, irei lhe ajudar a fazer o calculo da porcentagem da tua proxima compra')
-n1= float(input('Qual o valor do produto que pretende comprar (Ex: 10.99) ?: '))
-n2= float(input('Qual o valor de % de desconto a ser aplicado no produto ?: '))
-n3= n1*n2/100
-n4= n1 - n3
-print (f'O valor final do produto com valor de {n1}$ aplicado o desconto de {n2}% que é = a {n3}$ resulta em {n4}$ a ser pago')
