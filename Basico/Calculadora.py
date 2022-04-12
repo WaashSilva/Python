ad51146b0290d97598dad2433eb13a3ad72e3340
WaashSilva <98480388+WaashSilva@users.noreply.github.com>
2022-04-12 14:01:04 +0100

    Delete Calculadora.py

--- Basico/Calculadora.py
+++
@@ -1,9 +0,0 @@
-print ('selecione a opção desejada')
-s1= str('1-soma')
-s2= str('2-subtração')
-d= str('3-divisão')
-m= str('4-multiplicação')
-s= str (f'{s1}\n{s2}\n{d}\n{m}')
-print(s)
-o= int(input('Qual a opção ?: '))
-
